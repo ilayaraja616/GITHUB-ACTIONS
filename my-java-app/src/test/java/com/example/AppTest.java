1f37e1973947057a96e713883eda2672d9d8b75b
ilayaraja616 <ichockalingam@infoane.com>
2024-07-09 13:10:01 +0530

    Add files via upload

---
+++ my-java-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+// my-java-app/src/test/java/com/example/AppTest.java
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
